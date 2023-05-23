38d3c09c96ea4d186320f3b8c80732b6480d207e
randjg <randyjgunawan1@gmail.com>
2023-05-23 22:39:36 +0700

    Initial Commit

---
+++ FindMySpot/FindMySpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMySpotApp.swift
+//  FindMySpot
+//
+//  Created by Randy Julian on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FindMySpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
