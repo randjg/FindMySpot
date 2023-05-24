b71e64cbe6b99f124399bc5b318b34f80f9c2966
randjg <randyjgunawan1@gmail.com>
2023-05-24 20:41:44 +0700

    Add Color, Animation, Floor Text Field

--- FindMySpot/Views/ContentView.swift
+++ FindMySpot/Views/ContentView.swift
@@ -22,6 +22,8 @@ struct ContentView: View {
     @State private var isPark = true
     @State private var buttonTitle = "PARK HERE"
     @State private var showAlert = false
+    @State private var inputText = ""
+    @State private var savedText = ""
     
     private let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -47,7 +49,7 @@ struct ContentView: View {
                 Text("FindMySpot")
                     .font(.largeTitle)
                 .bold()
-                .padding(.leading, 20)
+                .padding(.leading, 16)
                 Spacer()
             }
             
@@ -65,7 +67,7 @@ struct ContentView: View {
                     ZStack {
                         ZStack {
                             RoundedRectangle(cornerRadius: 10)
-                                .foregroundColor(.blue)
+                                .foregroundColor(Color(hex: "004197"))
                             .frame(width: 393, height: 120)
                             .shadow(radius: 15)
                             
@@ -79,35 +81,45 @@ struct ContentView: View {
                         
                         ZStack {
                             RoundedRectangle(cornerRadius: 10)
-                                .foregroundColor(.white)
+                            .foregroundColor(.white)
                             .frame(width: 393, height: 100)
                             .padding(.top, 70)
-                        .shadow(radius: 10)
+                            .shadow(radius: 15)
                             
-                            Button(action: {
-                                if isPark && buttonTitle == "PARK HERE"{
-                                    buttonTitle = "FINISHED"
-                                    saveCoordinates()
-                                } else{
-                                    buttonTitle = "PARK HERE"
-                                    removeSavedCoordinate()
-                                }
-                            }) {
-                                Text(buttonTitle)
-                                    .font(.title2)
-                                    .fontWeight(.bold)
-                                    .frame(width: 300, height: 16)
-                                    .padding()
-                                    .background(Color.blue)
-                                    .foregroundColor(.white)
+                            HStack {
+                                TextField("Floor", text: $inputText)
+                                    .padding(.all)
+                                    .frame(width: 100, height: 48)
+                                    .background(Color(hex: "EFEFF0"))
                                     .cornerRadius(10)
-                            }
-                            .alert(isPresented: $showAlert){
-                                Alert(title: Text("FINISHED"),
-                                      message: Text("Click follow coordinate button on the top left to Park again"),
-                                      dismissButton: .default(Text("OK")))
-                            }
+                                    .foregroundColor(.black)
+                                    .padding(.top, 60)
+                                
+                                Button(action: {
+                                    if isPark && buttonTitle == "PARK HERE"{
+                                        buttonTitle = "FINISHED"
+                                        saveCoordinates()
+                                    } else{
+                                        buttonTitle = "PARK HERE"
+                                        removeSavedCoordinate()
+                                    }
+                                }) {
+                                    Text(buttonTitle)
+                                        .font(.title2)
+                                        .fontWeight(.bold)
+                                        .frame(width: 200, height: 16)
+                                        .padding()
+                                        .background(Color(hex: "00214E"))
+                                        .foregroundColor(.white)
+                                        .cornerRadius(10)
+                                }
+                                .alert(isPresented: $showAlert){
+                                    Alert(title: Text("FINISHED"),
+                                          message: Text("Click follow coordinate button on the top left to Park again"),
+                                          dismissButton: .default(Text("OK")))
+                                }
                             .padding(.top, 60)
+                            }
                         }
                     }
 
@@ -144,6 +156,17 @@ struct ContentView: View {
         UserDefaults.standard.set(location?.coordinate.longitude, forKey: "Longitude")
         loadCoordinates()
         
+        UserDefaults.standard.set(inputText, forKey: "SavedString")
+        retrieveSavedString()
+    }
+    
+    private func retrieveSavedString(){
+        if let savedString = UserDefaults.standard.string(forKey: "SavedString"){
+            savedText = savedString
+            print(savedText)
+        } else {
+            savedText = ""
+        }
     }
     
     private func loadCoordinates(){
@@ -168,9 +191,43 @@ struct ContentView: View {
             latitude = 0.0
             print("Saved latitude: \(latitude)")
         }
+        
+        if UserDefaults.standard.value(forKey: "SavedString") is String{
+            UserDefaults.standard.removeObject(forKey: "SavedString")
+            savedText = ""
+            inputText = ""
+            print(savedText)
+        }
         showAlert = true
     }
         
+}
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- FindMySpot/Views/SplashScreenView.swift
+++ FindMySpot/Views/SplashScreenView.swift
@@ -6,29 +6,45 @@
 //
 
 import SwiftUI
+import LottieUI
 
 struct SplashScreenView: View {
     
     @State private var isActive = false
     
     var body: some View {
-        NavigationView{
-            VStack{
-                Text("Find My Spot")
-                    .onAppear{
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 2){
-                            isActive = true
-                        }
+        NavigationStack{
+            
+            ZStack {
+                Rectangle()
+                    .foregroundColor(Color(hex: "B8E2F2"))
+                    .background(Color(hex: "B8E2F2"))
+                VStack{
+                    VStack {
+                        Image("logo")
+                            .resizable()
+                            .scaledToFit()
+                            .padding(.top, 50)
+                            .frame(width: 250, height: 250)
+                            .onAppear{
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+                                    isActive = true
+                                }
+                            }
+                            .background(
+                            NavigationLink(destination: ContentView(), isActive: $isActive){
+                            }
+                                .hidden()
+                        )
+//                        Image("splash")
+//                            .resizable()
+//                            .scaledToFit()
+                        LottieView("splash3")
+                            .loopMode(.playOnce)
                     }
-                    .background(
-                    NavigationLink(destination: ContentView(), isActive: $isActive){
-                        ContentView()
-                    }
-                        .hidden()
-                    )
+                }
             }
         }
-        
     }
 }
 
